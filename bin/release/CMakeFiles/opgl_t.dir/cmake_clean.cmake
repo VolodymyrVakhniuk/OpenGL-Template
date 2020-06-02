95e7b841d1fd2f82620bd3eae54fbfec0dcc946e
Volodymyr Vakhniuk <vvakhn2@uic.edu>
2020-06-02 15:40:15 +0300

    g

---
+++ bin/release/CMakeFiles/opgl_t.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opgl_t.dir/Source/Application.cpp.o"
+  "CMakeFiles/opgl_t.dir/Source/System/Context.cpp.o"
+  "CMakeFiles/opgl_t.dir/Source/main.cpp.o"
+  "opgl_t"
+  "opgl_t.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opgl_t.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
